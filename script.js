c0bd31485b3a643bc04b2a57dca5779ba636b98c
Obisanya1 <ifeoluwadaniel25@gmail.com>
2024-03-07 17:53:22 +0100

    test

---
+++ script.js
@@ -0,0 +1,20 @@
+let numbers = [6, 5, 3, 2];
+
+let addition1 = (numbers[0] + numbers [1]);
+
+
+let addition2 = (numbers[2] + numbers[3])
+
+
+let answer = (addition1 - addition2)
+
+
+
+function addnumbers(num1, num2, num3, num4) {
+console.log(
+    (num1 + num2) - (num3 + num4)
+)
+}
+
+addnumbers(6,5,3,2)
+
